1665edf3ff372d2250e110f8368b0f998c809600
seriesatul <atulsingh04895@gmail.com>
2025-11-09 22:15:33 +0530

    Made some changes in Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -93,7 +93,7 @@ const App = () => {
         {/* Optional Footer */}
         <footer className="bg-white border-t border-gray-200 py-4">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center text-sm text-gray-600">
-            <p>&copy; {new Date().getFullYear()} Your App Name. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} LinkedIn-Clone-By-Atul. All rights reserved.</p>
           </div>
         </footer>
       </div>
